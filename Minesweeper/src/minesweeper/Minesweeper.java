dc9cac42aafb20346a6c98809041afd38bc68ff5
kalavic <kalavic@Slagbuddy-1015PE>
2015-01-22 17:51:27 +0600

    Initial Commit

--- Minesweeper/src/minesweeper/Minesweeper.java
+++ Minesweeper/src/minesweeper/Minesweeper.java
@@ -5,17 +5,44 @@
  */
 package minesweeper;
 
+import java.util.Scanner;
+
 /**
  *
  * @author kalavic
  */
 public class Minesweeper {
-
+//Instance variables
+    String name;
+    String welcomeText = "Hello, this is the Minesweeper game.\n"
+            + " Press the button to proceed with the game.";
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Minesweeper myGame = new Minesweeper();
+        myGame.getName();
+        myGame.displayWelcome();
         // TODO code application logic here
     }
-    
+
+    /**
+     *
+     */
+    public void getName(){
+        // Getting the name of the player
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        this.name = input.next();
+    }
+
+    /**
+     *
+     */
+    public void displayWelcome() {
+        //Displaying wencome message
+        System.out.println("\nWelcome" + this.name + "\n");
+        System.out.println(this.welcomeText);
+    }
 }
