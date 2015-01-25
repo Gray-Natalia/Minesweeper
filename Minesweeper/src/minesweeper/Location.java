615c1f22b65ab6a4805f4c39e7bbf952009331e5
Alfonso Ramirez <ramvu275@gmail.com>
2015-01-24 22:55:45 -0600

    Location added

--- Minesweeper/src/minesweeper/Location.java
+++ Minesweeper/src/minesweeper/Location.java
@@ -5,29 +5,29 @@
  */
 package minesweeper;
 
-import javax.swing.JButton;  // Here location obtains the properties of a button
-
 /**
  *
  * @author Alfonso
  */
-public class Location extends JButton{  // Location becomes a button
-                
-        private boolean mine;     //Location will have a mine (true of false)
-        
-      
-        public Location() {   //Constructor
-            
-            super();  
-            double random = Math.random (); //To define randomly if it exist or not a mine
+public class Location {
+    int columnTotal;  //Number of columns
+    int rowTotal; //Number of rows
+    int squeres = columnTotal + rowTotal;
+    int numMines = (columnTotal*rowTotal)- squeres;
+    
+    boolean mines;
+    
+    
+    public Location (){
+    
+  
+    double random = Math.random (); //To define randomly if it exist or not a mine
             if (random>0.9)   //10% of probability to exist a mine
-                mine=true;
+                mines=true;
             else
-                mine=false;
-            
-        }
-        
-      public boolean isMined(){
-          return mine;
-      }  
+                mines=false;
+           
+           
+    }      
 }
+
