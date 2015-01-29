620216b1e62a26c2f8f92895e37e4a32f021df96
cheryl <cheryl@cheryl2>
2015-01-28 21:46:51 -0500

    Cheryl is working on her intial push to the minesweeper game

--- Minesweeper/src/minesweeper/Player.java
+++ Minesweeper/src/minesweeper/Player.java
@@ -1,15 +1,35 @@
-package minesweeper;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package minesweeper;
+
+import java.util.Scanner;
 
 /**
- * trying again
+ *
  * @author cheryl
+ * setup CLASS
  */
 public class Player {
-    
+ /***
+  * setup two instances for class PLAYER
+  */   
+String firstName;
+String lastName;
+String playerWelcome = "Please enter your first and last name. We hope you enjoy playing our version of Minesweeper";
+/****
+ * setup two functions so players can input their first and last names to play the game
+ */
+public void getFirstName() {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your first name: ");
+    this.firstName = input.next();  
+}
+public void getLastName(){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your last name: ");
+    this.lastName = input.next();
+}
 }
