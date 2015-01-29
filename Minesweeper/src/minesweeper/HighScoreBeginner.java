e0a96736e7844a3a0209779aed03dc940496a699
Keith <Keith@KB>
2015-01-28 20:24:06 -0700

    Initial Commit of High Score Beginners

---
+++ Minesweeper/src/minesweeper/HighScoreBeginner.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper;
+
+/**
+ *
+ * @author Keith
+ */
+public class HighScoreBeginner {
+    public static void main(String[] args) {
+        int[] ranking = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        String[] playerName = {"John", "James", "Jill", "Bill", 
+        "Kevin", "Nate", "Bob", "Dan", "Amy", "Jane" };
+        double[] time = { 10, 20, 30, 35, 40, 55, 65, 85, 93, 100 };
+    }
+}
