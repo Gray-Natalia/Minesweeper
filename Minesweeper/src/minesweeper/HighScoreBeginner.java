456b4ef02cc06b2804ceaa6e60ff5e13673814b0
Keith <Keith@KB>
2015-01-28 20:34:12 -0700

    Initial Commit of High Score Beginners

--- Minesweeper/src/minesweeper/HighScoreBeginner.java
+++ Minesweeper/src/minesweeper/HighScoreBeginner.java
@@ -14,6 +14,9 @@ public class HighScoreBeginner {
         int[] ranking = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         String[] playerName = {"John", "James", "Jill", "Bill", 
         "Kevin", "Nate", "Bob", "Dan", "Amy", "Jane" };
-        double[] time = { 10, 20, 30, 35, 40, 55, 65, 85, 93, 100 };
+        double[] time = { 10.5, 20.23, 30, 35, 40, 55, 65, 85, 93, 100 };
+        
+       
+        System.out.println(ranking[0] + "." + "\t" + playerName[0] + "\t" + time[0]);
     }
 }
