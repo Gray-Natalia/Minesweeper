ed13842ac8e4bf8ed15698c0b450c16fd7229eed
Alfonso Ramirez <ramvu275@gmail.com>
2015-01-29 00:02:14 -0600

    Location added

---
+++ Minesweeper/src/minesweeper/enterName.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper;
+
+/**
+ *
+ * @author Alfonso
+ */
+public class enterName {
+    public static void main (String args[]){
+       
+        char name; // The name of the player
+        int level; // Choose the level to play
+        
+        enterName beginner = new enterName();
+         name = 'x';
+    if (name <= 3) System.out.println ("name must be at least 3 character");
+    
+    
+        
+        
+    }
+       
+                
+                
+                
+                
+        
+        
+        
+        
+                
+        
+        
+    }
+
