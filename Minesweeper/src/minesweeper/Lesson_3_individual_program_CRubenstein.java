45bd7845384f55666d97d9d777338368cf6269e0
cheryl <cheryl@cheryl2>
2015-01-29 20:31:57 -0500

    Cheryl is working on her intial push to the minesweeper game

--- Minesweeper/src/minesweeper/Lesson_3_individual_program_CRubenstein.java
+++ Minesweeper/src/minesweeper/Lesson_3_individual_program_CRubenstein.java
@@ -1,72 +1,79 @@
-/*
+
+/***
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ ***/
 package minesweeper;
 
 import java.util.Scanner;
 
-
-/**
- *
+/***
  * @author cheryl
- */
+ ***/
 public class Lesson_3_individual_program_CRubenstein {
-  /***
-   * Lesson 3 individual assignment - two primitive variables, two math operators, /n
-   * one relational or logical operator, type casting and two character escape sequences
-   */
+        /***
+         * Lesson 3 individual assignment - two primitive variables, two math operators, /n
+         * one relational or logical operator, type casting and two character escape sequences
+         ***/
     String playerName;
-    String welcome = "Please enter a player name to begin game";
+    String welcome;
+        String level;
+        int num1;
+        int num2;
+        double answer;
+
+        Lesson_3_individual_program_CRubenstein() {
+                String welcome = "Please enter a player name to begin game";
+                String level = "Beginner";
+                num1 = (int)(Math.random()*10);
+                num2 = (int)(Math.random()*10);
+        }
+
     public void getPlayerName(){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your player name: ");
-        playerName = input.next();
+                Scanner input = new Scanner(System.in);
+                System.out.print("Enter your player name: ");
+                playerName = input.next();
     }
-     public void DisplayLevelInfo(){ 
+
+    public void DisplayLevelInfo(){
          System.out.println("Please choose level: Beginner, Intermediate or Advanced level");
-     }
-    String level = "Beginner, Intermediate, Advanced";
-           public void getLevel(){
-             Scanner input = new Scanner(System.in);
-        System.out.println("Enter your level: ");
+    }
+
+    public void getLevel(){
+                Scanner input = new Scanner(System.in);
+        System.out.print("Enter your level: ");
         level = input.next();
-        }
-           double answer;
-        public void getAnswer(){
-            Scanner input = new Scanner(System.in);
-          System.out.println("Enter your answer here: ");
-          answer = input.nextDouble();
-        }
-            public void DisplayQuestion(){
-                System.out.println("8 x 2 = ");
-            }
-          
-        public void checkAnswer(){
-            if(answer == 16){
- System.out.println ("Correct Answer! Great Job!!");
-}
-               else {
-                System.out.println ("Sorry, incorret answer. Try again?");
-                String response = "yes, no";
+    }
+
+    public void getAnswer(){
                 Scanner input = new Scanner(System.in);
-                System.out.println("Please enter yes or no: ");
-                    response = input.next();
-                     if (response == "yes"){
-                    System.out.println("8 x 2 = ");
-                    getAnswer();
-else System.out.println("The answer was 16. Next problem?");
-     string response = "yes, no"
-                Scanner input = new Scanner(System.in);
-                System.out.println("Please enter yes or no: ")
-                   if answer = yes;
-/* go back top need new problem
-*/
-else System.out.println ("Game over");  
-}
-else
-/* go back to top need new problem
-}
-*/
+                System.out.print("Enter your answer here: ");
+                answer = input.nextDouble();
+    }
+
+        public void displayQuestion(){
+                System.out.println(num1 + " x " + num2 + " = ");
+        }
+
+        public void checkAnswer() {
+                if(answer == num1 * num2) {
+                        System.out.println("Correct Answer! Great Job!!\n");
+                }
+                else {
+                        System.out.println("Sorry, incorrect answer.\n The answer was \"" + num1 * num2 + "\"");
+                }
+        }
+
+       /* public static void main(String args[]) {
+                Lesson_3_individual_program_CRubenstein ob1 = new Lesson_3_individual_program_CRubenstein();
+                ob1.displayQuestion();
+                ob1.getAnswer();
+                ob1.checkAnswer();
+
+                Lesson_3_individual_program_CRubenstein ob2 = new Lesson_3_individual_program_CRubenstein();
+                ob2.displayQuestion();
+                ob2.getAnswer();
+                ob2.checkAnswer();
+        }*/
 }
