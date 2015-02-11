7b73208e8869b44114d907fbc82af86802f35a42
knban_000 <knban_000@KB>
2015-02-10 19:58:43 -0800

    Group Assignment GetSelectedCell initial creation.

---
+++ Minesweeper/src/minesweeper/GetSelectedCell.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jacksonrkj
+ */
+public class GetSelectedCell {
+    
+    private Game game; 
+    
+    /*
+     * default constructor
+     */
+    public GetSelectedCell(Game game) {
+        this.game = game;
+        
+    } 
+    
+    public String getInput(Player player) {
+
+        String newMarker = null;
+        Scanner in = new Scanner(System.in);
+                
+        boolean valid = false; // flag to indicate if valid character entered
+        while (!valid) {
+            // prompt for input
+            System.out.println("\n\t" + player.name 
+                    + ", enter a single character that will be used to mark "
+                    + "your squares in the game.");
+            
+            // get input from user           
+            newMarker = in.nextLine();
+           
+            // no marker entered?
+            if (newMarker == null  || newMarker.length() < 1) {
+                continue;
+            }
+            
+            // grab only the first character and convert it to upper case
+            newMarker = newMarker.substring(0, 1).toUpperCase();
+            
+            if (newMarker.equals("Q")) { // Quit?
+                return null;
+            }
+            
+            // Check to see if the marker is already in use
+            if (this.game.playerA.marker.equals(newMarker) ||
+                this.game.playerB.marker.equals(newMarker) ) {
+               new TicTacToeError().displayError(
+                        "This marker is currently in use. " +
+                        "Select a different character");
+               continue; 
+            }
+            
+            valid = true; // signal that a valid marker was entered
+        }
+        
+        return newMarker;
+    }
+    
+}
