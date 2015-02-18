d00403fc588c86e9b6773158613947bf21a9cc4d
knban_000 <knban_000@KB>
2015-02-18 14:01:03 -0700

    InsertionSort class for Group assignment Lesson 6

--- Minesweeper/src/minesweeper/InsertionSort.java
+++ Minesweeper/src/minesweeper/InsertionSort.java
@@ -27,20 +27,11 @@ public class InsertionSort {
         {
             tempColumn = mineLocations.mineLocation [j].column;
             tempRow = mineLocations.mineLocation [j].row;
-            for(i = j - 1; (i >= 0) && (mineLocations.mineLocation[i].column >= tempColumn) ; i--) //Switched < to > to reverse order of output.
+            for(i = j - 1; (i >= 0 ) && (mineLocations.mineLocation[i].column > tempColumn|| 
+                    (mineLocations.mineLocation[i].column == tempColumn && mineLocations.mineLocation[i].row > tempRow)); i--)
             {
-                //Added if statement for if the column is greater (example column of C7 > column of A5 so condition is fulfilled)
-                if (mineLocations.mineLocation[i].column > tempColumn) {                            
-                    mineLocations.mineLocation[i+1].column = mineLocations.mineLocation[i].column;
-                    mineLocations.mineLocation[i+1].row = mineLocations.mineLocation[i].row;
-                }
-                //Else if column is equal (example column of C7 == column of C2, but rows are different.)
-                //So this checks the row values if the columns are the same. So since C7 and C2 are both C 
-                //it checks the number and orders them correctly (A5, B2, C2, C7, D3, E6, F8, G1, H9, I)
-                else if (mineLocations.mineLocation[i].column == tempColumn && mineLocations.mineLocation[i].row > tempRow) { 
-                    mineLocations.mineLocation[i+1].column = mineLocations.mineLocation[i].column;
-                    mineLocations.mineLocation[i+1].row = mineLocations.mineLocation[i].row;
-                }
+                mineLocations.mineLocation[i+1].column = mineLocations.mineLocation[i].column;
+                mineLocations.mineLocation[i+1].row = mineLocations.mineLocation[i].row;
             }
             mineLocations.mineLocation[i+1].column = tempColumn;
             mineLocations.mineLocation[i+1].row = tempRow;
