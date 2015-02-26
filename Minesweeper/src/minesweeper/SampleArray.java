2fa981955b7f57b5455b2376cef8f91b253c3352
knban_000 <knban_000@KB>
2015-02-25 19:01:36 -0700

    Integrating all classes into existing menus.

---
+++ Minesweeper/src/minesweeper/SampleArray.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper;
+
+/**
+ *
+ * @author knban_000
+ */
+public class SampleArray {
+    public static void main(String args[]) {
+        int[] sampleArray = new int[10];
+        sampleArray[0] = 3;
+        sampleArray[1] = 13;
+        sampleArray[2] = 6;
+        sampleArray[3] = 9;
+        sampleArray[4] = 33;
+        sampleArray[5] = 6;
+        sampleArray[6] = 97;
+        sampleArray[7] = 5;
+        sampleArray[8] = 3;
+        sampleArray[9] = 23;
+        
+        
+        
+        
+    
+        for (int i = 0; i < sampleArray.length; i++) {
+            System.out.println(sampleArray[i]);
+        }
+    }
+}
