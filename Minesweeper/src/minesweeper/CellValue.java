d3cd088c87dd7115725ee04e27096f1bac48c7c1
cheryl <cheryl@cheryl2>
2015-03-03 19:09:45 -0500

    Individual Lab for lesson 8 CIT 260-05 - Cheryl Rubenstein

--- Minesweeper/src/minesweeper/CellValue.java
+++ Minesweeper/src/minesweeper/CellValue.java
@@ -5,18 +5,83 @@
  */
 package minesweeper;
 
+import java.io.Serializable;
+
 /**
  *
  * @author knban_000
  */
-public class CellValue {
-    public char column;
-    public int row;
-    public int cellValue; //value of 10 means mine.
+public class CellValue implements Serializable {
+    private char column;
+    private int row;
+    private int cellValue; //value of 10 means mine.
     public CellValue(char col, int ro, int cv) {
         
         column = col;
         row = ro;
         cellValue = cv;
     }
+
+    public CellValue() {
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public void setColumn(char column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCellValue() {
+        return cellValue;
+    }
+
+    public void setCellValue(int cellValue) {
+        this.cellValue = cellValue;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 47 * hash + this.column;
+        hash = 47 * hash + this.row;
+        hash = 47 * hash + this.cellValue;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CellValue other = (CellValue) obj;
+        if (this.column != other.column) {
+            return false;
+        }
+        if (this.row != other.row) {
+            return false;
+        }
+        if (this.cellValue != other.cellValue) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CellValue{" + "column=" + column + ", row=" + row + ", cellValue=" + cellValue + '}';
+    }
+    
 }
