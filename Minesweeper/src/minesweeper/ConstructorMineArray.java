9f9eb5a906d2dae1c0b669869d588319bd7f3524
cheryl <cheryl@cheryl2>
2015-03-03 19:23:30 -0500

    Individual Lab for lesson 8 CIT 260-05 - Cheryl Rubenstein

--- Minesweeper/src/minesweeper/ConstructorMineArray.java
+++ Minesweeper/src/minesweeper/ConstructorMineArray.java
@@ -5,16 +5,70 @@
  */
 package minesweeper;
 
+import java.io.Serializable;
+
 /**
  *
  * @author knban_000
+ * convert to java bean by C Rubenstein
  */
-public class ConstructorMineArray {
-    public char column;
-    public int row;
+public class ConstructorMineArray implements Serializable {
+    private char column;
+    private int row;
     
     public ConstructorMineArray(char col, int ro) {
         column = col;
         row = ro;
     }
+
+    public ConstructorMineArray() {
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public void setColumn(char column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + this.column;
+        hash = 29 * hash + this.row;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ConstructorMineArray other = (ConstructorMineArray) obj;
+        if (this.column != other.column) {
+            return false;
+        }
+        if (this.row != other.row) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ConstructorMineArray{" + "column=" + column + ", row=" + row + '}';
+    }
+    
 }
