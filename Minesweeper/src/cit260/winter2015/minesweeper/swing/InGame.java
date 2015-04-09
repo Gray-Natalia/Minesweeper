b538edf9686d598114dd5ddc95252eaa0f0a33ae
knban_000 <knban_000@KB>
2015-04-09 00:18:04 -0600

    Working on GUI

--- Minesweeper/src/cit260/winter2015/minesweeper/swing/InGame.java
+++ Minesweeper/src/cit260/winter2015/minesweeper/swing/InGame.java
@@ -22,6 +22,7 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 import javax.swing.ImageIcon;
@@ -206,8 +207,8 @@ public class InGame extends javax.swing.JPanel {
         img = new ImageIcon[LevelType.NUM_IMAGES];
         
         for (int i = 0; i < 16; i++) {
-            img[i] = new ImageIcon("images/" + i + ".png");
-            
+//            InputStream inStream = this.getClass().getClassLoader().getResourceAsStream("resources/" + i + ".png");
+            img[i] = new ImageIcon("resources/" + i + ".png");
         }
     }
 
